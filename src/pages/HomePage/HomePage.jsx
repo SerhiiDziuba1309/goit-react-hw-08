1282efed57339ec356e0b24b1103df12f0a2179f
SerhiiDziuba1309 <doktorschuuu@gmail.com>
2024-12-12 19:40:54 +0100

    finish

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import PhoneIcon from '@mui/icons-material/Phone';
 import s from './HomePage.module.css';
 
 const HomePage = () => (
   <div className={s.homePage}>
-    <h1 className={s.title}>
-      <PhoneIcon className={s.icon} /> Welcome to Phonebook App
-    </h1>
+    <h1 className={s.title}>Welcome to Phonebook App</h1>
     <p className={s.description}>
       Manage your contacts efficiently and securely.
     </p>
